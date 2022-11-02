b9c8c69f389847220014d53e83c77ea7f079ea4f
StephVelito <svelitoruiz@gmail.com>
2022-11-02 11:47:09 -0500

    React

---
+++ react-basico/github-project/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import HeaderSection from "./components/HeaderSection";
+import Userinformation from "./components/UserInformation";
+import SunSection from "./components/SearchContainer";
+import Swal from "sweetalert2";
+import "./App.css";
+import SunSection from "./components/SunSection";
+
+const App = () => {
+  const [inputText, setInputText] = useState("");
+
+  const [user, setUser] = useState(null);
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  async function searchUser() {
+    try {
+      const response = await fetch(`https://api.github.com/users/${inputText}`);
+      const data = await response.json();
+
+      if (data.message === "Not Found") {
+        Swal.fire("Error", "El usuario que buscas no existe", "error");
+        return;
+      }
+
+      setUser(data);
+      setInputText("");
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+
+  return (
+    <div className="container">
+      <HeaderSection />
+      <SunSection
+        inputText={inputText}
+        handleInputChange={handleInputChange}
+        searchUser={searchUser}
+      />
+      {/* aca podemos hacer una validacion donde digamos que el div que sigue exista siempre y cuando user tengo datos */}
+      {user && <Userinformation user={user} />}
+    </div>
+  );
+};
+
+export default App;
