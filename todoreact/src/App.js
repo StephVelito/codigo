9023662fe686fe7a7a70670e9935f2d40b8414a6
StephVelito <svelitoruiz@gmail.com>
2022-11-03 20:09:12 -0500

    con alerta

--- todoreact/src/App.js
+++ todoreact/src/App.js
@@ -1,32 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TaskForm, TaskCard } from "./components";
+import { get, post, update } from "./services";
 
 function App() {
-	const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState([]);
 
-	function addTask(text) {
-		const newTask = {
-			text: text,
-			datetime: new Date(),
-		};
-		setTaskList([...taskList, newTask]);
-		console.log(taskList);
-	}
+  async function getTasks() {
+    const tasks = await get();
+    setTaskList(tasks);
+  }
 
-	return (
-		<div className="container my-5">
-			<h1 className="display-3">Todo App</h1>
-			<TaskForm onSubmitFunction={addTask} />
-			<div className="text-primary">
-				<hr />
-			</div>
-			<div className="mt-5">
-				{taskList.map((task, index) => (
-					<TaskCard key={index} task={task} />
-				))}
-			</div>
-		</div>
-	);
+  async function addTask(text) {
+    const newTask = { name: text, status: 1 };
+    await post(newTask);
+    await getTasks();
+  }
+
+  async function updateTask(id) {
+    const body = { status: 2 };
+    await update(id, body);
+    await getTasks();
+  }
+
+  useEffect(() => {
+    getTasks();
+    // que provoca el []: Que se ejecute solo una vez
+  }, []);
+
+  return (
+    <div className="container my-5">
+      <h1 className="display-3">Todo App</h1>
+      <TaskForm onSubmitFunction={addTask} />
+      <div className="text-primary">
+        <hr />
+      </div>
+      <div className="mt-5">
+        {taskList.map((task, index) => (
+          <TaskCard
+            key={index}
+            task={task}
+            getTasks={getTasks}
+            updateTask={updateTask}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
