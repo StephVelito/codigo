0f95603da83407dba8483cf5770c042c1836dd12
StephVelito <svelitoruiz@gmail.com>
2022-11-03 23:07:47 -0500

    profile

--- todoreact/src/services/index.js
+++ todoreact/src/services/index.js
@@ -4,62 +4,71 @@ const url = "https://6363105f66f75177ea3c935b.mockapi.io/tareas";
 // POST: Crear
 // PUT: Actualizar
 // DELETE: Eliminar
-
-export const get = async () => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const post = async (body) => {
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(body),
-    });
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// id: Tarea
-export const update = async (id, body) => {
-  try {
-    const response = await fetch(`${url}/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(body),
-    });
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// Para la eliminar un registro unicamente necesito el id
-export const destroy = async (id) => {
+export const getProfile = async () => {
     try {
-      const response = await fetch(`${url}/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+        const response = await fetch ("https://api.github.com/users/stephvelito");
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.log("Error", error);
+      }
+    };
+    
+    export const get = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+    export const post = async (body) => {
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(body),
+        });
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+    // id: Tarea
+    export const update = async (id, body) => {
+      try {
+        const response = await fetch(`${url}/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(body),
+        });
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+    // Para la eliminar un registro unicamente necesito el id
+    export const destroy = async (id) => {
+      try {
+        const response = await fetch(`${url}/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
